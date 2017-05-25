09c179b01cda9dc9a0c46eacbf39ca324a2d94fb
axv680 <axv680@C83160.hanover.com>
2017-05-24 23:27:28 -0400

    New method

--- Demoproject/src/test/java/com/test/demo/Hello.java
+++ Demoproject/src/test/java/com/test/demo/Hello.java
@@ -38,5 +38,11 @@ public class Hello {
 	@Test
 	public void testmethod6 (){
 				System.out.println("Hello world 6");
+                           	}
+	
+	@Test
+	public void testmethod7 (){
+				System.out.println("Hello world 7");
 			                  }
 }
+
